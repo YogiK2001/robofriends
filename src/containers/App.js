c3ef8b63e281ad119bb48e0f7c80df30f259b852
YogiK2001 <kaustubh7718@gmail.com>
2024-08-05 00:17:22 +0530

    hooks update

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,47 +1,65 @@
 import React from "react";
-import { Component } from "react";
+//  import { Component } from "react";
 import CardList from "../components/CardList"
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundary";
+import { useState, useEffect } from "react";
 import './App.css';
 
+// Upgrading to React 18 and using Hooks instead of Component
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state =
-        {
-            robots: [],
-            searchField: ''
-        }
-    }
+function App() {
+    // constructor() {
+    //     super();
+    //     this.state =
+    //     {
+    //         robots: [],
+    //         searchField: ''
+    //     }
+    // }
 
-    componentDidMount() {
+    // Added Hooks
+    const [robots, setRobot] = useState([]);
+    const [searchField, setSearchField] = useState('');
+
+    // componentDidMount() {
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //         .then(Response => Response.json())
+    //         .then(users => this.setState({ robots: users }));
+    // } Instead of this we will use useEffect Hook
+
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
-            .then(Response => Response.json())
-            .then(users => this.setState({ robots: users }));
-    }
+          .then(Response => Response.json())
+          .then(users => setRobot(users));
+        }, []); // Empty array means it will run only once
+
+
+
     
-    onSearchChange = (event) => {
+    const onSearchChange = (event) => {
         /*Cannot read properties of undefined (reading 'setState') will come if your write 
         like onSearchChange(event) becasue this is will look for "setState" in the event
         instead of the class */
-        this.setState({ searchField: event.target.value });
+        // this.setState({ searchField: event.target.value });
+        setSearchField(event.target.value);
     }
 
-    render() {
-        const { robots, searchField } = this.state;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchField.toLowerCase());
-        });
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+
+    
+        // const { robots, searchField } = this.state;
+        
         return !robots.length ? <h1>Loading</h1> 
         :
         (
             <div className="tc" >
                 <h1 className="f1" >RoboFriends</h1>
                 <h4 className="f3" >Find Your Robot Friends Here!</h4>
-                <SearchBox searchChange={this.onSearchChange} />
+                <SearchBox searchChange={onSearchChange} />
                 <Scroll>
                     <ErrorBoundary>
                     <CardList robots={filteredRobots} />
@@ -50,5 +68,4 @@ class App extends Component {
             </div>
         );
     }
-}
 export default App;
